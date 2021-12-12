644c811d7551f071f41a3a85fb5b68e40937b5bf
Nitwel <nitwel@arcor.de>
2021-12-12 15:10:30 +0100

    change socket server

--- website/src/setup.ts
+++ website/src/setup.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import mitt from 'mitt'
 // import { ElNotification } from 'element-plus'
 
-export const socket = io(import.meta.env.PROD ? "playcah.de:5000" : "localhost:5000")
+export const socket = io(`${location.protocol}//${location.host}:5000`)
 export const emitter = mitt()
 
 export function notify(title: string, message: string, type: 'info' | 'error' | 'success' | 'warning' = "info") {
